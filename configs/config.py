eb7b761d0650097d2bc342f68b7ed01bf7ae1a82
Hienvq <Neih4207@gmail.com>
2022-09-25 02:04:58 +0700

    add feature extraction config

---
+++ configs/config.py
@@ -0,0 +1,10 @@
+from src.utils import dotdict
+
+FeatureExtraction = {
+    'sample-path':  "/Users/mac/Desktop/AI/VLSP-Speech-Recognization/data/A-MSV-TrainingData 7/MSV_Youtube_data/vi/_PNXexBriFs/6.wav", 
+    "n-fft": 1024,
+    "win-length": None,
+    "hop-length": 512,
+    "n-mel": 128,
+    "n-mfcc": 128 
+}
